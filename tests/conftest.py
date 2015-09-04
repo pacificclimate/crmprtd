fa5359262521f32472cd9d0d68507e68602bc9bd
Basil Veerman <bveerman@uvic.ca>
2015-09-04 13:04:54 -0700

    Add test moti xml with full var set

--- tests/conftest.py
+++ tests/conftest.py
@@ -151,3 +151,85 @@ def moti_sawr7110_new_station():
     </observation-series>
   </data>
 </cmml>''')
+
+def moti_sawr7100_large():
+    return '''<?xml version="1.0" encoding="ISO-8859-1"?>
+<cmml xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="..\Schema\CMML.xsd" version="2.01">
+  <head>
+    <product operational-mode="official">
+      <title>Observation from BC Meteorological Stations</title>
+      <field>meteorological</field>
+      <category>observation</category>
+      <creation-date refresh-frequency="PT1H">2015-09-03T22:11:04-07:00</creation-date>
+    </product>
+    <source>
+      <production-center>British Columbia Ministry of Transportation<sub-center>AWP</sub-center></production-center>
+    </source>
+  </head>
+  <data>
+    <observation-series>
+      <origin type="station">
+        <id type="client">11091</id>
+        <id type="network">BC_MoT_11091</id>
+      </origin>
+      <observation valid-time="2011-04-07T01:00:00-07:00">
+        <pressure index="1" type="atmospheric">
+          <value units="mb">949</value>
+        </pressure>
+        <wind index="1" type="average-scalar-speed-over-60minutes">
+          <value units="km/h">2.422</value>
+        </wind>
+        <wind index="1" type="average-direction">
+          <value units="deg">6.875</value>
+        </wind>
+        <wind index="1" type="standard-deviation-of-direction-over-60minutes">
+          <value units="deg">21.01</value>
+        </wind>
+        <temperature index="1" type="air-temperature">
+          <value units="degC">-.813</value>
+        </temperature>
+        <temperature index="1" type="dew-point">
+          <value units="degC">-1</value>
+        </temperature>
+        <precipitation index="1" type="total-over-hour">
+          <value units="mm">0</value>
+        </precipitation>
+        <pavement index="1" type="temperature">
+          <qualifier units="unitless" type="lane-number">1</qualifier>
+          <value units="degC">1.7</value>
+        </pavement>
+        <pavement index="2" type="temperature">
+          <qualifier units="unitless" type="lane-number">1</qualifier>
+          <value units="degC">2.6</value>
+        </pavement>
+        <pavement index="1" type="freeze-point">
+          <qualifier units="unitless" type="lane-number">1</qualifier>
+          <value units="degC">-21.1</value>
+        </pavement>
+        <pavement index="1" type="surface-status">
+          <qualifier type="categorical-table" units="string">BC-MoT-pavement-surface-condition-code</qualifier>
+          <value units="code">24</value>
+        </pavement>
+        <subsurface index="1" type="temperature">
+          <qualifier units="unitless" type="lane-number">1</qualifier>
+          <qualifier units="cm" type="sensor-depth">25</qualifier>
+          <value units="degC">6.7</value>
+        </subsurface>
+        <humidity index="1" type="relative-humidity">
+          <value units="%">98</value>
+        </humidity>
+        <snow index="1" type="snowfall-accumulation-rate">
+          <value units="cm">-.016</value>
+        </snow>
+        <snow index="1" type="adjacent-snow-depth">
+          <value units="cm">97.8</value>
+        </snow>
+        <extension index="2">
+          <qualifier units="string" type="name">bcmot-precipitation-detection-ratio</qualifier>
+          <value units="unitless">.079</value>
+        </extension>
+      </observation>
+    </observation-series>
+  </data>
+</cmml>
+'''
