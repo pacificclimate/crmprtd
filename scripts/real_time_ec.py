2d95415c22743a95408564b5ebf7c0d666f17c64
Basil Veerman <bveerman@uvic.ca>
2015-10-01 15:18:27 -0700

    Change EC scripts to argparse

--- scripts/real_time_ec.py
+++ scripts/real_time_ec.py
@@ -4,10 +4,10 @@
 import os, sys
 import logging, logging.config
 from datetime import datetime, timedelta
-from optparse import OptionParser
+from argparse import ArgumentParser
 from urllib import urlopen
 from traceback import print_exc
-from pkg_resources import resource_stream
+from pkg_resources import resource_filename
 
 # Installed libraries
 from psycopg2 import InterfaceError, ProgrammingError, OperationalError
@@ -20,32 +20,40 @@ from crmprtd.ec import makeurl, ObsProcessor
 # debug
 from pdb import set_trace
 
-def main(opts, args):
+def main(args):
     # Setup logging
-    log_conf = yaml.load(opts.log_conf)
-    if opts.log:         log_conf['handlers']['file']['filename'] = opts.log
-    else:                opts.log = log_conf['handlers']['file']['filename']
-    if opts.error_email: log_conf['handlers']['mail']['toaddrs'] = opts.error_email
+    with open(args.log_conf, 'rb') as f:
+        log_conf = yaml.load(f)
+    if args.log:
+        log_conf['handlers']['file']['filename'] = args.log
+    else:
+        args.log = log_conf['handlers']['file']['filename']
+    if args.error_email:
+        log_conf['handlers']['mail']['toaddrs'] = args.error_email
     logging.config.dictConfig(log_conf)
+    log = logging.getLogger('crmprtd.wmb')
+    log.info('Starting WMB rtd')
+    if args.log_level:
+        log.setLevel(args.log_level)
 
     auto = True
     try:
-        if opts.filename:
-            logging.debug("Opening local xml file {0} for reading".format(opts.filename))
+        if args.filename:
+            logging.debug("Opening local xml file {0} for reading".format(args.filename))
             auto = False
-            fname = opts.filename
-            xml_file = open(opts.filename, 'r')
+            fname = args.filename
+            xml_file = open(args.filename, 'r')
             logging.debug("File opened sucessfully")
         else:
-            if opts.time:
-                opts.time = datetime.strptime(opts.time, '%Y/%m/%d %H:%M:%S')
-                logging.info("Starting manual run using timestamp {0}".format(opts.time))
+            if args.time:
+                args.time = datetime.strptime(args.time, '%Y/%m/%d %H:%M:%S')
+                logging.info("Starting manual run using timestamp {0}".format(args.time))
                 auto = False
             else:
-                deltat = timedelta(1/24.) if opts.frequency == 'hourly' else timedelta(1) # go back a day
-                opts.time = datetime.utcnow() - deltat
-                logging.info("Starting automatic run using timestamp {0}".format(opts.time))
-            url = makeurl(opts.frequency, opts.province, opts.language, opts.time)
+                deltat = timedelta(1/24.) if args.frequency == 'hourly' else timedelta(1) # go back a day
+                args.time = datetime.utcnow() - deltat
+                logging.info("Starting automatic run using timestamp {0}".format(args.time))
+            url = makeurl(args.frequency, args.province, args.language, args.time)
             fname = url['filename']
             logging.info("Downloading {0}".format(url['url']))
             xml_file = urlopen(url['url'])
@@ -57,7 +65,7 @@ def main(opts, args):
     # instantiate the ObsProcessor (do the startup stuff, like opening db connection and parsing the XML)
     try:
         logging.info("Instantiating the ObsProcessor",)
-        op = ObsProcessor(xml_file, opts)
+        op = ObsProcessor(xml_file, args)
         logging.info("Done setting up ObsProcessor")
     except (LxmlSyntaxError, IOError, OperationalError), e:
         if type(e) == OperationalError:
@@ -68,9 +76,9 @@ def main(opts, args):
             logging.exception("Failed to open xml file\n{0}".format(xml_file))
         # Save data for reprosessing
         if auto:
-            fname = os.path.join(opts.cache_dir, 'failure-' + fname)
+            fname = os.path.join(args.cache_dir, 'failure-' + fname)
         else:
-            fname = os.path.join(opts.cache_dir, 'manual_run_' + datetime.now().strftime('%Y%m%dT%H%M%S') + '_failure-' + fname)
+            fname = os.path.join(args.cache_dir, 'manual_run_' + datetime.now().strftime('%Y%m%dT%H%M%S') + '_failure-' + fname)
         logging.info("Saving data at: {}".format(fname))
         f = open(fname, 'w')
         f.write(xml_file.read())
@@ -78,7 +86,7 @@ def main(opts, args):
         logging.critical('''Critical errors have occured in the EC real time downloader that require a human touch.
         The daemon was unable to either download, open, or parse the incoming xml and no observations could be inserted.
         Please consult the log file at {log}.
-        Data has been archived at {f}.'''.format(log=opts.log, f=fname))
+        Data has been archived at {f}.'''.format(log=args.log, f=fname))
         sys.exit(1)
 
     # process the XML and do the db insertions
@@ -90,40 +98,45 @@ def main(opts, args):
         logging.exception("Unhandleable exception, saving remaining XML file {} for further examination".format(fname))
     finally:
         if auto:
-            remainder = os.path.join(opts.cache_dir, 'remainder-' + fname)
+            remainder = os.path.join(args.cache_dir, 'remainder-' + fname)
         else:
-            remainder = os.path.join(opts.cache_dir, 'manual_run_' + datetime.now().strftime('%Y%m%dT%H%M%S') + '_remainder-' + fname)
+            remainder = os.path.join(args.cache_dir, 'manual_run_' + datetime.now().strftime('%Y%m%dT%H%M%S') + '_remainder-' + fname)
         op.save(remainder)
         logging.info("Data saved at {}".format(remainder))
 
 if __name__ == '__main__':
 
-    parser = OptionParser()
-    parser.add_option('-c', '--connection_string', dest='connection_string', help='PostgreSQL connection string of form:\n\tdialect+driver://username:password@host:port/database\nExamples:\n\tpostgresql://scott:tiger@localhost/mydatabase\n\tpostgresql+psycopg2://scott:tiger@localhost/mydatabase\n\tpostgresql+pg8000://scott:tiger@localhost/mydatabase')
-    parser.add_option('-y', '--log_conf', dest='log_conf', help='YAML file to use to override the default logging configuration')
-    parser.add_option('-l', '--log', dest='log', help="log filename")
-    parser.add_option('-e', '--error_email', dest='error_email', help='e-mail address to which the program should report error which require human intervention')
-    parser.add_option('-C', '--cache_dir', dest='cache_dir', help='directory in which to put the downloaded file in the event of a post-download error')
-    parser.add_option('-f', '--filename', dest='filename', help='MPO-XML file to process')
-    parser.add_option('-p', '--province', dest='province', help='2 letter province code')
-    parser.add_option('-L', '--language', dest='language', help="'e' (english) | 'f' (french)")
-    parser.add_option('-F', '--frequency', dest='frequency', help='daily|hourly')
-    parser.add_option('-t', '--time', dest='time', help="Alternate time to use for downloading (interpreted with strptime(format='%Y/%m/%d %H:%M:%S')")
-    parser.add_option('-T', '--threshold', dest='thresh', help='Distance threshold to use when matching stations.  Stations are considered a match if they have the same id, name, and are within this threshold')
-    parser.add_option('-D', '--diag', dest='diag', action="store_true", help="Turn on diagnostic mode (no commits)")
-    # parser.add_option('-o', '--output_dir', dest='output_dir', help='directory in which to put the downloaded file')
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--connection_string', required=True,
+                         help='PostgreSQL connection string of form:\n\tdialect+driver://username:password@host:port/database\nExamples:\n\tpostgresql://scott:tiger@localhost/mydatabase\n\tpostgresql+psycopg2://scott:tiger@localhost/mydatabase\n\tpostgresql+pg8000://scott:tiger@localhost/mydatabase')
+    parser.add_argument('-y', '--log_conf',
+                         default=resource_filename('crmprtd', '/data/logging.yaml'),
+                         help='YAML file to use to override the default logging configuration')
+    parser.add_argument('-l', '--log',
+                         help="log filename")
+    parser.add_argument('--log_level',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help='Set log level: DEBUG, INFO, WARNING, ERROR, CRITICAL.  Note that debug output by default goes directly to file')
+    parser.add_argument('-e', '--error_email',
+                        help='e-mail address to which the program should report error which require human intervention')
+    parser.add_argument('-C', '--cache_dir', required=True,
+                        help='directory in which to put the downloaded file in the event of a post-download error')
+    parser.add_argument('-f', '--filename',
+                        help='MPO-XML file to process')
+    parser.add_argument('-p', '--province',  required=True,
+                        help='2 letter province code')
+    parser.add_argument('-L', '--language', default='e',
+                        choices=['e', 'f'],
+                        help="'e' (english) | 'f' (french)")
+    parser.add_argument('-F', '--frequency', required=True,
+                        choices=['daily', 'hourly'],
+                        help='daily|hourly')
+    parser.add_argument('-t', '--time',
+                        help="Alternate *UTC* time to use for downloading (interpreted using format=YYYY/MM/DD HH:MM:SS)")
+    parser.add_argument('-T', '--threshold', dest='thresh', default=1000,
+                        help='Distance threshold to use when matching stations.  Stations are considered a match if they have the same id, name, and are within this threshold')
+    parser.add_argument('-D', '--diag', default=False, action="store_true",
+                        help="Turn on diagnostic mode (no commits)")
 
-    parser.set_defaults(connection_string='dbname=rtcrmp user=crmp',
-                        log_conf = resource_stream('crmprtd', '/data/logging.yaml'),
-                        log=None,
-                        error_email='hiebert@uvic.ca',
-                        cache_dir='/home/data/projects/crmp/data/EC_Downloads/auto',
-                        #filename='/tmp/hourly_bc_2011111416_e.xml',
-                        filename=None,
-                        province='BC', language='e', frequency='daily',
-                        time=None,
-                        diag=False,
-                        thresh=0
-                        )
-    (opts, args) = parser.parse_args()
-    main(opts, args)
+    args = parser.parse_args()
+    main(args)
