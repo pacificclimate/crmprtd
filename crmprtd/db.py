609dd5b4507324c1db4eebe75bb7a8f670ed3003
James Hiebert <hiebert@uvic.ca>
2017-06-21 11:39:31 -0700

    Adds a mass-insert strategy for obs

---
+++ crmprtd/db.py
@@ -0,0 +1,55 @@
+from math import floor
+import logging
+
+import psycopg2
+
+
+def split(tuple_):
+    if len(tuple_) < 1:
+        return (), ()
+    elif len(tuple_ < 2):
+        return (tuple_[0], ())
+    else:
+        i = floor(len(tuple_) / 2)
+        return (tuple_[:i], tuple_[i:])
+
+
+def mass_insert_obs(sesh, obs, log=None):
+    '''This function implements a recursive Obs insert strategy to
+       handle unique constraint errors on members of the set. The
+       strategy used is to optimistically attempt to insert the entire
+       set and in the event of a unique constraint error, it will
+       divide the set into two and try again on each set (which will
+       presumably have a lower probability of failing).
+
+       In the degenerative case (all observations are duplicates), this
+       strategy will require up to n *additional* transactions,
+       but in the optimal case it reduces the transactions to a constant
+       1.
+    '''
+    if not log:
+        log = logging.getLogger(__name__)
+
+    # Base cases
+    if len(obs) < 1:
+        pass
+    elif len(obs == 1):
+        sesh.add(obs[0])
+        try:
+            sesh.commit()
+        except psycopg2.IntegrityError as e:
+            log.debug("%s %s", obs, e)
+        else:
+            return 1
+
+    # The happy case: add everything at once
+    else:
+        sesh.add_all(obs)
+        try:
+            sesh.commit()
+            return len(obs)
+        except psycopg2.IntegrityError:
+            a, b = split(obs)
+            return mass_insert_obs(sesh, a, log) + \
+                mass_insert_obs(sesh, b, log)
+    return 0
