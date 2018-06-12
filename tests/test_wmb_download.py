ae57afdc721e26424f4635928f2db347f3c9f044
Nikola Rados <nrados@uvic.ca>
2018-06-12 16:06:48 -0700

    Update test

--- tests/test_wmb_download.py
+++ tests/test_wmb_download.py
@@ -2,4 +2,5 @@ from crmprtd.wmb.download import logging_setup
 
 def test_logging_setup():
     log = logging_setup('mof.log', 'test@mail.com', 'INFO')
-    assert log != None
+    assert log.name == 'crmprtd.wmb'
+    assert log.level == 20
