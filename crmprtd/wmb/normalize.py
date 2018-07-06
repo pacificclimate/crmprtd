9cc735e435610672856cf99e739bd2296fac04d2
Nikola Rados <nrados@uvic.ca>
2018-07-06 10:57:37 -0700

    Add datetime type to date param

--- crmprtd/wmb/normalize.py
+++ crmprtd/wmb/normalize.py
@@ -15,7 +15,6 @@ from crmprtd import Row
 
 def normalize(file_stream):
     log = logging.getLogger(__name__)
-    tz = pytz.timezone('Canada/Pacific')
 
     var_names = ['station_code',
                 'weather_date',
@@ -62,6 +61,7 @@ def normalize(file_stream):
                 parsed_date += '{}:00'.format(date[8:10])
 
             # convert types where necessary
+            tz = pytz.timezone('Canada/Pacific')
             try:
                 cleaned_date = parse(parsed_date).replace(tzinfo=tz)
             except ValueError:
@@ -75,7 +75,7 @@ def normalize(file_stream):
                 continue
 
             # create namedTuple
-            named_row = Row(time=parsed_date,
+            named_row = Row(time=cleaned_date,
                 val=val,
                 variable_name=key,
                 unit=None,
@@ -85,34 +85,3 @@ def normalize(file_stream):
                 lon=None)
 
             yield named_row
-
-    #
-    # for row in file_stream.readlines():
-    #     cleaned = row.strip().replace('"', '').split(',')
-    #     # loop through all variables
-    #     for i,var in enumerate(var_row, 2):
-    #         # check if var has value
-    #         if len(cleaned[i]) == 0:
-    #             continue
-    #
-    #         # parse date
-    #         date = cleaned[1]
-    #         parsed_date = date[0:4] + '-' + date[4:6] + '-' + date[6:8] + ' '
-    #         if date[8:10] == '24':
-    #             parsed_date = parsed_date + '00'
-    #         else:
-    #             parsed_date = parsed_date + date[8:10]
-    #         cleaned_date = parse(parsed_date).replace(tzinfo=tz)
-    #
-    #         try:
-    #             named_row = Row(time=cleaned_date,
-    #                 val=float(cleaned[i]),
-    #                 variable_name=var,
-    #                 unit=None,
-    #                 network_name='WMB',
-    #                 station_id=cleaned[0],
-    #                 lat=None,
-    #                 lon=None)
-    #             yield named_row
-    #         except Exception as e:
-    #             log.error('Unable to process row: {}'.format(row))
