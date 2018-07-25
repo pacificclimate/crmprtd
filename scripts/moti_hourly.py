1a63702d90cb871bf5d35257b958f43a0d46535e
Nikola Rados <nrados@uvic.ca>
2018-07-25 16:46:36 -0700

    Change arg names where overlap

--- scripts/moti_hourly.py
+++ scripts/moti_hourly.py
@@ -14,37 +14,6 @@ from crmprtd import iterable_to_stream
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-c', '--connection_string',
-                        help=('PostgreSQL connection string of form:'
-                              '\n\tdialect+driver://username:password@host:'
-                              'port/database\n'
-                              'Examples:'
-                              '\n\tpostgresql://scott:tiger@localhost/'
-                              'mydatabase'
-                              '\n\tpostgresql+psycopg2://scott:tiger@'
-                              'localhost/mydatabase'
-                              '\n\tpostgresql+pg8000://scott:tiger@localhost'
-                              '/mydatabase'))
-    parser.add_argument('-y', '--log_conf',
-                        default=resource_stream(
-                            'crmprtd', '/data/logging.yaml'),
-                        help=('YAML file to use to override the default '
-                              'logging configuration'))
-    parser.add_argument('-l', '--log', help="log filename")
-    parser.add_argument('-e', '--error_email',
-                        help=('e-mail address to which the program should '
-                              'report error which require human intervention'))
-    parser.add_argument('--log_level',
-                        choices=['DEBUG', 'INFO',
-                                 'WARNING', 'ERROR', 'CRITICAL'],
-                        help=('Set log level: DEBUG, INFO, WARNING, ERROR, '
-                              'CRITICAL.  Note that debug output by default '
-                              'goes directly to file'))
-    parser.add_argument('-C', '--cache_file',
-                        help=('file in which to put the downloaded data '
-                              'in the event of a post-download error'))
-    parser.add_argument('-f', '--filename',
-                        help='MPO-XML file to process')
     parser.add_argument('-S', '--start_time',
                         help=("Alternate time to use for downloading "
                               "(interpreted with "
@@ -53,7 +22,7 @@ if __name__ == '__main__':
                         help=("Alternate time to use for downloading "
                               "(interpreted with "
                               "strptime(format='Y/m/d H:M:S')"))
-    parser.add_argument('-i', '--station_id',
+    parser.add_argument('-s', '--station_id',
                         help="Station ID for which to download data")
     parser.add_argument('--auth',
                         help="Yaml file with plaintext usernames/passwords")
@@ -66,11 +35,7 @@ if __name__ == '__main__':
     parser.add_argument('--bcidpass',
                         help=("The BCID password for data requests. Overrides "
                               "auth file."))
-    parser.add_argument('-D', '--diag', action="store_true", default=False,
-                        help="Turn on diagnostic mode (no commits)")
-    # parser.add_argument('-o', '--output_dir', dest='output_dir',
-    #                     help='directory in which to put the downloaded file')
-
+    parser = common_script_arguments(parser)
     args = parser.parse_args()
     log = logging_setup(args.log_conf, args.log,
                         args.error_email, args.log_level)
