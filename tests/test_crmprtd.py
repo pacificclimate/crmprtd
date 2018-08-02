e58b7f44ccc35aae7e926e1c01d3413ad4f4531b
James Hiebert <james@hiebert.name>
2018-08-02 05:54:00 -0700

    PEP8 fixes

--- tests/test_crmprtd.py
+++ tests/test_crmprtd.py
@@ -2,6 +2,7 @@ import pytest
 
 from crmprtd import subset_dict
 
+
 @pytest.mark.parametrize(('a_dict', 'keys', 'expected'), (
     ({'a': 'b', 'c': 'd', 'e': 'f'}, ['a', 'e'], {'a': 'b', 'e': 'f'}),
     ({'a': 'b'}, ['a'], {'a': 'b'}),
