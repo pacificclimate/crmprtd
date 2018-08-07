5bf4266f9b712aea365cdfcdb0d93b461814666e
Nikola Rados <nrados@uvic.ca>
2018-08-07 15:23:06 -0700

    Move speed testing to own directory

--- crmprtd/insert.py
+++ crmprtd/insert.py
@@ -1,5 +1,5 @@
-"""insert.py
 
+"""insert.py
 The insert module handles the Insert phase of the crmprtd
 pipeline. This phase simply consists of doing a mass insert of
 observations into the database. The phase needs to handle any unique
@@ -8,115 +8,11 @@ failures. This phase needs to manage the database transactions for
 speed and reliability. This phase is common to all networks.
 """
 
-from pycds import Obs
-import datetime
-from crmprtd import Timer
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
-from sqlalchemy import and_
-from crmprtd.wmb_exceptions import UniquenessError, InsertionError
-from sqlalchemy.exc import IntegrityError
-import statistics
 
-
-def insert_one_by_one(sesh, o):
-    # Check to see if this entry will be unique
-    try:
-        q = sesh.query(Obs.id).filter(
-            and_(Obs.history_id == o.history_id, Obs.vars_id == o.vars_id, Obs.time == o.time))
-        if q.count() > 0:
-            # print("Failure, observation already exists.")
-            raise UniquenessError(q.first())
-    except UniquenessError as e:
-        # print("Failure, observation already exists.")
-        # print(e)
-        raise e
-    except Exception as e:
-        # print("Failure, an error occured.")
-        # print(e)
-        raise InsertionError(obs_time=o.time, datum=o.datum,
-                             vars_id=o.vars_id, hid=o.history_id, e=e)
-
-    # value does not exist in obs_raw, continue with insertion
-    try:
-        sesh.add(o)
-        sesh.rollback()
-        ## print("Successfully inserted observation")
-        return 1
-    except Exception as e:
-        # print("Failure, an error occured.")
-        # print(e)
-        raise InsertionError(obs_time=o.time, datum=o.datum,
-                             vars_id=o.vars_id, hid=o.history_id, e=e)
-
-
-def insert_mass(sesh, os):
-    # Base cases
-    if len(os) < 1:
-        return 0
-    elif len(os) == 1:
-        try:
-            # Create a nested SAVEPOINT context manager to rollback to in the
-            # event of unique constraint errors
-            # print("New SAVEPOINT for single observation")
-            with sesh.begin_nested():
-                sesh.add(os[0])
-        except IntegrityError as e:
-            # print("Failure, observation already exists. os: {}, exception: {}".format(os, e))
-            sesh.rollback()
-            print('HERE')
-            return 0
-        else:
-            # print("Success for single observation")
-            sesh.rollback()
-            return 1
-
-    # The happy case: add everything at once
-    else:
-        try:
-            with sesh.begin_nested():
-                ## print("New SAVEPOINT. num_obs:{}".format(len(os)))
-                print('has to')
-                sesh.add_all(os)
-        except IntegrityError:
-            # print("Failed, splitting observations.")
-            sesh.rollback()
-            a, b = split(os)
-            # print("Splitings observations into a, b.")
-            print('maybe')
-            a, b = insert_mass(sesh, a), insert_mass(sesh, b)
-            combined = a + b
-            # print("Returning from split call")
-            return a + b
-        else:
-            # print("Successfully inserted observations")
-            sesh.rollback()
-            print('ill be pissed')
-            return len(os)
-
-
-def insert_with_one_by_one(db, obs):
-    Session = sessionmaker(create_engine(db))
-    sesh = Session()
-
-    for ob in obs:
-        try:
-            insert_one_by_one(sesh, ob)
-        except:
-            pass
-
-def chunks(list, chunk_size):
-    for i in range(0, len(list), chunk_size):
-        yield list[i:i+chunk_size]
-
-
-def insert_with_chunks(db, obs, chunk_size):
-    Session = sessionmaker(create_engine(db))
-    sesh = Session()
-
-    for chunk in chunks(obs, chunk_size):
-        try:
-            insert_mass(sesh, chunk)
-        except:
-            pass
+def insert(sesh, observations):
+    successes, skips, failures = 0, 0, 0
+    return {
+        'successes': successes,
+        'skips': skips,
+        'failures': failures
+    }
