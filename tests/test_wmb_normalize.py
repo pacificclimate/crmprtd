8f27bb0bc74f3543d50182bb4471467de31a9963
Nikola Rados <nrados@uvic.ca>
2018-08-13 13:19:21 -0700

    Fix tests where hour offset occurs

--- tests/test_wmb_normalize.py
+++ tests/test_wmb_normalize.py
@@ -14,7 +14,7 @@ def test_normalize_good_data():
     assert len(rows) == 17
     for row in rows:
         assert row.station_id == '11'
-        assert row.time == datetime.strptime('2018052711',
+        assert row.time == datetime.strptime('2018052710',
                                              "%Y%m%d%H").replace(tzinfo=tz)
         assert row.variable_name is not None
         assert row.val is not None
@@ -23,7 +23,7 @@ def test_normalize_good_data():
 
 def test_normalize_bad_date():
     lines = '''station_code,weather_date,precipitation,temperature,relative_humidity,wind_speed,wind_direction,ffmc,isi,fwi,rn_1_pluvio1,snow_depth,snow_depth_quality,precip_pluvio1_status,precip_pluvio1_total,rn_1_pluvio2,precip_pluvio2_status,precip_pluvio2_total,rn_1_RIT,precip_RIT_Status,precip_RIT_total,precip_rgt,solar_radiation_LICOR,solar_radiation_CM3
-11,BAD_DATE,.00,14.2,55,10.4,167,81.160995,2.1806495,5.5260615,.00,.00,,,.00,.00,,.00,.00,.00,.00,,.0,
+11,2018052799,.00,14.2,55,10.4,167,81.160995,2.1806495,5.5260615,.00,.00,,,.00,.00,,.00,.00,.00,.00,,.0,
 ''' # noqa
     rows = [row for row in normalize(StringIO(lines))]
     assert len(rows) == 0
@@ -38,7 +38,7 @@ def test_normalize_bad_value():
     tz = pytz.timezone('Canada/Pacific')
     for row in rows:
         assert row.station_id == '11'
-        assert row.time == datetime.strptime('2018052711',
+        assert row.time == datetime.strptime('2018052710',
                                              "%Y%m%d%H").replace(tzinfo=tz)
         assert row.variable_name is not None
         assert row.val is not None
