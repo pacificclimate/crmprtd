0d17a49cd8e260fabb89111981343422c83fd47e
Nikola Rados <nrados@uvic.ca>
2018-08-23 09:53:15 -0700

    Move script responsibilities to download

--- crmprtd/ec/download.py
+++ crmprtd/ec/download.py
@@ -3,12 +3,15 @@ import sys
 import logging
 import logging.config
 from datetime import datetime, timedelta
+from argparse import ArgumentParser
+import pickle
 
 # Installed libraries
 import requests
 
 # Local
 from crmprtd.ec import makeurl
+from crmprtd import common_script_arguments, setup_logging, logging_args
 
 log = logging.getLogger(__name__)
 
@@ -43,8 +46,39 @@ def download(time, frequency, province, language):
         if req.status_code != 200:
             raise IOError(
                 "HTTP {} error for {}".format(req.status_code, req.url))
-        return req.iter_content(chunk_size=None)
+
+        pickle.dump('Network module name: ec', sys.stdout.buffer)
+        for line in req.iter_content(chunk_size=None):
+            pickle.dump(line, sys.stdout.buffer)
 
     except IOError:
         log.exception("Unable to download or open xml data")
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--province', required=True,
+                        help='2 letter province code')
+    parser.add_argument('-g', '--language', default='e',
+                        choices=['e', 'f'],
+                        help="'e' (english) | 'f' (french)")
+    parser.add_argument('-F', '--frequency', required=True,
+                        choices=['daily', 'hourly'],
+                        help='daily|hourly')
+    parser.add_argument('-t', '--time',
+                        help=("Alternate *UTC* time to use for downloading "
+                              "(interpreted using "
+                              "format=YYYY/MM/DD HH:MM:SS)"))
+    parser.add_argument('-T', '--threshold', default=1000,
+                        help=('Distance threshold to use when matching '
+                              'stations.  Stations are considered a match if '
+                              'they have the same id, name, and are within '
+                              'this threshold'))
+    parser = logging_args(parser)
+    args = parser.parse_args()
+
+    setup_logging(args.log_conf, args.log_filename, args.error_email,
+                  args.log_level, 'crmprtd.ec')
+
+    download(args.time, args.frequency, args.province, args.language)
