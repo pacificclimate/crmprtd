e673e8e7f4974a39f3ca4ff351b457706aec1eb1
nikola-rados <nrados@uvic.ca>
2020-06-30 10:00:35 -0700

    Mark slow tests

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -1,10 +1,13 @@
 from pycds import Contact, Network
+import pytest
 
 
+@pytest.mark.slow
 def test_can_instantiate(test_session):
     print("I can haz enjun")
 
 
+@pytest.mark.slow
 def test_db_has_data(test_session):
     q = test_session.query(Contact.name)
     assert set([rv[0] for rv in q.all()]) == set(['Simon', 'Pat', 'Eric'])
@@ -13,6 +16,7 @@ def test_db_has_data(test_session):
         ['MoTIe', 'EC_raw', 'FLNRO-WMB', 'ENV-AQN'])
 
 
+@pytest.mark.slow
 def test_db_has_geo(postgis_session):
     res = postgis_session.execute(
         ("SELECT ST_AsText(ST_GeomFromText('POLYGON((0 0,0 1,1 1,1 0,0 0))'"
@@ -20,6 +24,7 @@ def test_db_has_geo(postgis_session):
     assert res.fetchall()[0][0] == 'POLYGON((0 0,0 1,1 1,1 0,0 0))'
 
 
+@pytest.mark.slow
 def test_db_has_binary(postgis_session):
     res = postgis_session.execute(
         ("SELECT ST_AsBinary(ST_GeomFromText('POLYGON((0 0,0 1,1 1,1 0,0 0))',"
--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -1,9 +1,10 @@
 import logging
 import logging.config
-
+import pytest
 from pycds import Network
 
 
+@pytest.mark.slow
 def test_nested_transactions_1(crmp_session):
     fake_network = Network(name='Fake Network')
 
@@ -18,6 +19,7 @@ def test_nested_transactions_1(crmp_session):
         raise e
 
 
+@pytest.mark.slow
 def test_nested_transactions_2(crmp_session):
     fake_network = Network(name='Fake Network')
     with crmp_session.begin_nested():
@@ -26,6 +28,7 @@ def test_nested_transactions_2(crmp_session):
         logging.debug('added moti network')
 
 
+@pytest.mark.slow
 def test_nested_transactions_3(crmp_session):
     fake_network = Network(name='Fake Network')
     try:
