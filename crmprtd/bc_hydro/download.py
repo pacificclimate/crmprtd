85775829f494bfbbd2d108ad8162f1b443e1d227
cairosanders <cairosanders@hotmail.com>
2020-09-16 12:50:40 -0700

    fix formatting errors

--- crmprtd/bc_hydro/download.py
+++ crmprtd/bc_hydro/download.py
@@ -1,13 +1,5 @@
-'''Downloads data from BC hyrdo
-'''
+# Downloads data from BC hyrdo
 
-# Standard module
-import logging
-import logging.config
-import sys
-from argparse import ArgumentParser
-
-#used
 import pysftp
 from tempfile import TemporaryDirectory, SpooledTemporaryFile
 import os
@@ -16,7 +8,6 @@ from zipfile import ZipFile
 from crmprtd import logging_args, setup_logging, common_auth_arguments
 
 log = logging.getLogger(__name__)
-
 
 def download(username, auth_key, ftp_server, ftp_dir):
     # Connect FTP server and retrieve file
@@ -43,7 +34,7 @@ def download(username, auth_key, ftp_server, ftp_dir):
                                 os.remove(os.getcwd() + '/' + txt_file)
                         
                     except:                    
-                        #not a zip file
+                        # Not a zip file
                         pass
                         
                 tempfile.seek(0)
@@ -59,8 +50,7 @@ def main():
     parser = common_auth_arguments(parser)
     parser.add_argument('-f', '--ftp_server',
                         default='sftp2.bchydro.com',
-                        help=('Full uri to BC Hydro\'s ftp '
-                              'server'))
+                        help=('Full uri to BC Hydro\'s ftp server')
     parser.add_argument('-F', '--ftp_dir',
                         default=('pcic'),
                         help='FTP Directory containing BC hydro\'s data files')
@@ -70,7 +60,6 @@ def main():
                   args.log_level, 'crmprtd.bc_hydro')
 
     download(args.username, args.auth_key, args.ftp_server, args.ftp_dir)
-
 
 if __name__ == "__main__":
     main()
