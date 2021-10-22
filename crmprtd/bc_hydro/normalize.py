cceb53cc3bab0fc6c7cbeb1ef98b05afaabb0f83
James Hiebert <james@hiebert.name>
2021-10-22 10:17:31 -0700

    Completes draft of BCHydro normalize module

--- crmprtd/bc_hydro/normalize.py
+++ crmprtd/bc_hydro/normalize.py
@@ -1,12 +1,11 @@
+import pdb
 import sys
+from itertools import islice, chain
 
 from dateutil import parser as date_parse
 from sly import Lexer, Parser
 
-
-class Empty(object):
-    def __repr__(self):
-        return "Empty"
+from crmprtd import Row
 
 
 class DataLexer(Lexer):
@@ -29,20 +28,31 @@ class DataLexer(Lexer):
         t.value = float(t.value)
         return t
 
+    def error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        self.index += 1
 
-class DataParser(Parser):
+
+class BCHydroExtendedCSV(Parser):
     tokens = DataLexer.tokens
 
-    @_("station_name header_line data_lines")
+    def __init__(self):
+        self.variables = []
+
+    @_("station_name bch_csv", "bch_csv")
     def extended_bch_csv(self, p):
-        pass
+        return p.bch_csv
 
     @_("header_line data_lines")
     def bch_csv(self, p):
-        pass
+        return chain.from_iterable(p.data_lines)
 
     @_("WORD NEWLINE")
     def station_name(self, p):
+        """This is the station name in the header, but the station names are
+        also listed in each data line. So we don't really care about
+        this
+        """
         pass
 
     @_("header_names NEWLINE")
@@ -51,19 +61,47 @@ class DataParser(Parser):
 
     @_("header_name", "header_names header_name")
     def header_names(self, p):
-        pass
+        self.variables.append(p.header_name)
 
     @_("WORD")
     def header_name(self, p):
-        return p[0]
+        return p.WORD
 
-    @_("data_line", "data_lines data_line")
+    @_("data_line", "data_line data_lines")
     def data_lines(self, p):
-        pass
+        # According to this grammer, and empty NEWLINE is a valid data line
+        # Trim this out if we see it.
+        line = [p.data_line] if p.data_line else []
+
+        if hasattr(p, "data_lines"):
+            return line + p.data_lines
+        else:
+            return line
 
     @_("WORD DATE data_values NEWLINE", "NEWLINE")
     def data_line(self, p):
-        return
+        if not hasattr(p, "WORD"):
+            return
+
+        station_id = p.WORD
+        datetime = p.DATE
+        rows = [
+            Row(
+                time=datetime,
+                val=value,
+                variable_name=variable_name,
+                network_name="BCH",
+                station_id=station_id,
+                unit=None,
+                lat=None,
+                lon=None,
+            )
+            for variable_name, value in islice(
+                zip(self.variables, p.data_values), 2, None
+            )
+            if value is not None
+        ]
+        return rows
 
     @_("data_value", "data_values data_value")
     def data_values(self, p):
@@ -77,26 +115,34 @@ class DataParser(Parser):
     @_("NUMBER", "EMPTY")
     def data_value(self, p):
         if p[0] == "+":
-            return Empty()
+            return None
         return p[0]
 
     def error(self, t):
         import pdb
 
         pdb.set_trace()
+        return
 
 
 if __name__ == "__main__":
     stream = open(sys.argv[1]).read()
     lexer = DataLexer()
-    parser = DataParser()
+    parser = BCHydroExtendedCSV()
+
+    tokens = lexer.tokenize(stream)
 
     if sys.argv[2] == "parse":
         try:
-            print(parser.parse(lexer.tokenize(stream)))
+            try:
+                rows = parser.parse(tokens)
+            except Exception as e:
+                pdb.set_trace()
+            for row in rows:
+                print(row)
         except EOFError:
-            pass
+            print("EOFERROR")
 
     else:
-        for token in lexer.tokenize(stream):
+        for token in tokens:
             print(token)
