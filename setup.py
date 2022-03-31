fa553415154c5ae782cf39110893606b9c385b5d
James Hiebert <james@hiebert.name>
2022-03-31 11:51:15 -0700

    py36 is EOL

--- setup.py
+++ setup.py
@@ -76,8 +76,8 @@ setup(
     cmdclass={"test": PyTest},
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
