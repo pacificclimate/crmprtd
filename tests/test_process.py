87d882056706439c87af28c5b663a62752adccc3
Marie Whibley <mariewhibley@uvic.ca>
2022-06-23 15:49:01 -0700

    Fix connection string

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,29 +1,27 @@
-import sys
-import io
 import os
-import re
 import pytz
 import pytest
 import logging
 from datetime import datetime
-from lxml import etree
 
+from pycds import Network
 from crmprtd.download import verify_date
 from crmprtd.process import process
 
 
 @pytest.mark.parametrize(
-    ("start_date", "end_date", "num_obs"),
+    ("start_date", "end_date", "num_inserts"),
     [
-        ("2022/06/1", "2022/06/18", 1643),
+        ("2022/06/1", "2022/06/18", 197),
         ("June 18", None, 0),
         (None, "June 17 2022 10am", 0),
-        (None, None, 1643),
+        (None, None, 197),
     ],
 )
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_process_date(start_date, end_date, num_obs, monkeypatch, caplog):
-
+def test_process_date(
+    start_date, end_date, num_inserts, monkeypatch, caplog, crmp_session
+):
     caplog.set_level(logging.INFO)
 
     utc = pytz.utc
@@ -35,22 +33,25 @@ def test_process_date(start_date, end_date, num_obs, monkeypatch, caplog):
     forestry_data = open(
         os.path.join(script_dir, "..", "crmprtd", "data", "forestry_data")
     )
-
     monkeypatch.setattr("sys.stdin", forestry_data)
 
+    crmp_session.add(Network(name="FLNRO-WMB"))
+    crmp_session.commit()
+
     process(
-        "postgresql://monsoon.pcic.uvic.ca:5432/crmp",
+        crmp_session.get_bind().url,
         50,
         "bc_forestry",
         start_date,
         end_date,
+        False,
         True,
     )
 
     assert (
         sum(
-            bool(re.search("<pycds.Obs object at 0x[a-z0-9]+>", str(record)))
+            bool("Successfully inserted observations" in str(record))
             for record in caplog.records
         )
-        == num_obs
+        == num_inserts
     )
