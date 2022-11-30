bbe22153c8746d1de6bcaffb37ba2087464a6a10
rod-glover <rod.glover.ca@gmail.com>
2022-11-30 11:27:23 -0800

    Raise exception if attempt to insert variables

--- crmprtd/infer.py
+++ crmprtd/infer.py
@@ -2,14 +2,14 @@
 
 The infer module is an optional phase of the crmprtd pipeline. This
 phase takes a sequence of Obs tuples and reports on what database
-metadata modifications would need to be made in aggreate to support
+metadata modifications would need to be made in aggregate to support
 the set of observations. This could include the creation of new
-variables, the creation of new stations and/or history entries.
+variables, stations and/or history entries.
 
 A common use case for this module would be when bringing a new network
 online, one needs to create database entries for variables to be
 tracked in this network. This can't/shouldn't necessarily be done in a
-completely automatically for a couple of reasons:
+completely automatically for several reasons:
 
 1. Many variables reported by a station are only necessary for
    operational purposes and not for necessarily for climate monitoring
@@ -23,6 +23,10 @@ completely automatically for a couple of reasons:
    analysis), long form descriptions, or other aliases
    (e.g. display_name).
 
+3. As of version 4.0.0 of PyCDS, several columns in the variables table
+   are not nullable and values cannot be derived for them (see point above).
+   Therefore it is no longer possible to insert variables with null values
+   for these columns. An attempt to insert such variables now raises an exception.
 """
 
 import logging
@@ -42,15 +46,15 @@ def create_variable(
     unit,
     standard_name=None,
     display_name=None,
-    cell_method="undefined",
+    cell_method=None,
 ):
     network = sesh.query(Network).filter(Network.name == network_name).one()
 
     return Variable(
         network=network,
         name=variable_name,
-        standard_name=standard_name or variable_name,
-        display_name=display_name or variable_name,
+        standard_name=standard_name,
+        display_name=display_name,
         cell_method=cell_method,
         unit=unit,
     )
@@ -81,7 +85,15 @@ def infer(sesh, obs_tuples, diagnostic=False):
     # diagnostic mode.
     with sesh.begin_nested() as nested:
         variables = [
-            create_variable(sesh, network_name, var_name, unit)
+            create_variable(
+                sesh,
+                network_name,
+                var_name,
+                unit,
+                standard_name="requires_human_intervention",
+                display_name="requires_human_intervention",
+                cell_method="requires_human_intervention",
+            )
             for network_name, var_name, unit in vars_to_create
             if not get_variable(sesh, network_name, var_name)
         ]
@@ -94,10 +106,14 @@ def infer(sesh, obs_tuples, diagnostic=False):
 
         if diagnostic:
             nested.rollback()
-        else:
-            sesh.add_all(vars_to_create)
-            nested.commit()
+        elif len(variables) > 0:
+            raise ValueError(
+                f"{len(variables)} Variables need to be inserted (see log). "
+                f"This is not possible without human intervention."
+            )
 
+    # TODO: Should this come before Variable processing so that anything it does (e.g.,
+    #   log information) happens regardless of whether an exception is raised above?
     # Reduce observations to unique set of tuples describing required histories
     # and stations.
     hists_to_create = {
