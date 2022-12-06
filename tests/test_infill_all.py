18012ec148e82b654273ab32c5f4f5a0fc2b5cf2
rod-glover <rod.glover.ca@gmail.com>
2022-12-06 09:56:00 -0800

    Update conn string for test_infill_all.py

--- tests/test_infill_all.py
+++ tests/test_infill_all.py
@@ -10,7 +10,7 @@ def test_download_and_process(mocker, caplog):
     mocker.patch("subprocess.run", return_value=True)
     with caplog.at_level(logging.DEBUG):
         download_and_process(
-            [], "moti", "postgresql://user:password@db3.pcic/somdb", []
+            [], "moti", "postgresql://user:password@db.pcic/somdb", []
         )
     for record in caplog.records:
         assert "password" not in record.message
