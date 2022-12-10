7d51dd4633355b4eb91f7cc8d9a8de1aa56d6aed
rod-glover <rod.glover.ca@gmail.com>
2022-12-10 15:04:44 -0800

    Clean up test_process.py

--- tests/test_process.py
+++ tests/test_process.py
@@ -5,7 +5,7 @@ import logging
 from datetime import datetime
 from pkg_resources import resource_filename
 
-from pycds import Network, Variable
+from pycds import Network, Variable, Obs
 from crmprtd.download import verify_date
 from crmprtd.process import process
 
@@ -29,35 +29,42 @@ def get_num_obs_inserted(records):
     return sum(get_per_rec(record) for record in records)
 
 
+def num_observations_in_db(session):
+    return session.query(Obs).count()
+
+
 @pytest.mark.parametrize(
-    ("start_date", "end_date", "num_inserts"),
+    # There are 2510 observations in the test data file, all dated 2022-06-17.
+    # Of these, 2360 are unique. Those should be inserted; the 150 duplicates should
+    # be skipped.
+    ("start_date", "end_date", "expected_num_inserts"),
     [
-        ("2022/06/1", "2022/06/18", 2510),
+        ("2022/06/1", "2022/06/18", 2360),
         ("June 18", None, 0),
         (None, "June 17 2022 10am", 0),
-        (None, None, 2510),
+        (None, None, 2360),
     ],
 )
 @pytest.mark.filterwarnings("ignore::UserWarning")
 def test_process_by_date(
-    start_date, end_date, num_inserts, monkeypatch, caplog, crmp_session
+    start_date, end_date, expected_num_inserts, monkeypatch, caplog, crmp_session
 ):
-    caplog.set_level(logging.INFO)
+    # Restrict the logging to just what is important to this test.
+    caplog.set_level(logging.WARNING, "sqlalchemy.engine")
+    caplog.set_level(logging.DEBUG, "crmprtd")
 
     utc = pytz.utc
 
     start_date = utc.localize(verify_date(start_date, datetime.min, "start date"))
     end_date = utc.localize(verify_date(end_date, datetime.max, "end date"))
 
-    forestry_data = open(resource_filename("crmprtd", "data/forestry_data.xml"))
-    monkeypatch.setattr("sys.stdin", forestry_data)
-
-    # Insert the network in the test data
+    # Insert the network required in the test data
     network = Network(name="FLNRO-WMB")
     crmp_session.add(network)
 
     # Insert the variables required in the test data.
-    # Note: Infer mode cannot insert variables, so we have to do it manually.
+    # Note: Infer mode does not insert variables, so we have to do it manually here,
+    # parallelling what we do in real usage.
     crmp_session.add_all(
         [
             Variable(
@@ -84,8 +91,13 @@ def test_process_by_date(
             )
         ]
     )
-
     crmp_session.commit()
+
+    num_obs_in_db_before = num_observations_in_db(crmp_session)
+
+    # Set up input from test data file.
+    forestry_data = open(resource_filename("crmprtd", "data/forestry_data.xml"))
+    monkeypatch.setattr("sys.stdin", forestry_data)
 
     process(
         connection_string=crmp_session.get_bind().url,
@@ -97,11 +109,9 @@ def test_process_by_date(
         do_infer=True,
     )
 
-    num_obs_to_insert = get_num_obs_to_insert(caplog.records)
-    num_obs_inserted = get_num_obs_inserted(caplog.records)
-    print(
-        f"num_obs_to_insert = {num_obs_to_insert}; "
-        f"num_obs_inserted = {num_obs_inserted}"
-    )
-    assert num_obs_to_insert == num_inserts
-    assert num_obs_inserted == num_obs_to_insert
+    num_obs_in_db_after = num_observations_in_db(crmp_session)
+    num_obs_inserted_in_db = num_obs_in_db_after - num_obs_in_db_before
+    log_num_obs_inserted = get_num_obs_inserted(caplog.records)
+
+    assert num_obs_inserted_in_db == expected_num_inserts
+    assert log_num_obs_inserted == expected_num_inserts
