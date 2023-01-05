5f8f2d512b5360fbdc3de39478c23d417965d0ab
rod-glover <rod.glover.ca@gmail.com>
2023-01-04 16:38:45 -0800

    Extract metnorth.sh from hourly_swobml2_newdb.sh

--- crmprtd/execution/hourly_swobml2_newdb.sh
+++ crmprtd/execution/hourly_swobml2_newdb.sh
@@ -15,17 +15,3 @@ for net in ${networks}; do
     echo "download_${net} -L logging.yaml --log_filename ${log_file} -d \"${TIME}\" | tee ${cache_file} |\
       crmprtd_process -c postgresql://crmprtd@db.pcic.uvic.ca:5433/crmp -N ${net} -L logging.yaml --log_filename ${log_file}" | batch
 done
-
-
-# As of April 14, 2022 The YT/NWT networks don't have their variables
-# in the database, so we can't insert the data, but we can at least
-# download the cache file and insert them later.
-
-export networks="nt_forestry nt_water yt_gov yt_water yt_avalanche yt_firewx"
-for net in ${networks}; do
-    export log_file=~/${net}/logs/${net}_json.log
-    export cache_file=~/${net}/cache/${net}_$(date +'%Y-%m-%dT%H:%M:%S').xml
-    echo "download_${net} -L ~/logging.yaml --log_filename ${log_file} -d \"${TIME}\" | tee ${cache_file} |\
-       crmprtd_process -c postgresql://crmprtd@dbnorth/metnorth -N ${net} -L ~/logging.yaml --log_filename ${log_file}" | batch
-
-done
--- crmprtd/execution/metnorth.sh
+++ crmprtd/execution/metnorth.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# Arguments:
-#   $1: override for location of crmprtd scripts
 
-# If the first argument ($1) is supplied, put it on the front of PATH.
-#[ ! -z "$1" ] && export PATH="$1:$PATH"
+# Distinguishes cache and log file names.
+tag="$1"
 
-export PATH="~/env_4.0.0/bin:$PATH"
-echo $PATH
-crmprtd_process --version
+export TIME=$(date -u "+%Y/%m/%d %H:00:00" -d "last hour");
 
 export networks="nt_forestry nt_water yt_gov yt_water yt_avalanche yt_firewx"
 for net in ${networks}; do
-    export log_file=~/${net}/logs/${net}_json.log
-    export cache_file=~/${net}/cache/${net}_$(date +'%Y-%m-%dT%H:%M:%S').xml
+    export log_file=~/${net}/logs/${tag}_{net}_json.log
+    export cache_file=~/${net}/cache/${tag}_{net}_$(date +'%Y-%m-%dT%H:%M:%S').xml
     echo "download_${net} -L ~/logging.yaml --log_filename ${log_file} -d \"${TIME}\" | tee ${cache_file} |\
-       crmprtd_process -c postgresql://crmprtd@dbnorth/metnorth2 -N ${net} -L ~/logging.yaml --log_filename ${log_file}" | batch
+       crmprtd_process -c postgresql://crmprtd@dbnorth/metnorth -N ${net} -L ~/logging.yaml --log_filename ${log_file}" | batch
 
 done
