104ec8c5014523b95ff17b2804ff75abe7b5dc9b
rod-glover <rod.glover.ca@gmail.com>
2023-01-04 16:14:14 -0800

    Remove unused scripts

--- crmprtd/execution/wmb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export net=wmb
-export NOW=$(date +'%Y-%m-%dT%H:%M:%S')
-
-export log_file=~/${net}/logs/mof_json.log
-export cache_file=~/${net}/download/${net}_${NOW}.txt
-
-echo "download_${net} --auth_fname ~/.rtd_auth.yaml --auth_key=${net} -L ~/logging.yaml --log_filename ${log_file} | tee ${cache_file} | crmprtd_process -c postgresql://crmprtd@monsoon/crmp -N ${net} -L ~/logging.yaml --log_filename ${log_file}" | batch
