ced1b3365aa686720cad2e953056503e1089ea5d
James Hiebert <james@hiebert.name>
2025-06-13 15:03:36 -0700

    Update expected tests results for new behaviour

--- crmprtd/tests/test_ec.py
+++ crmprtd/tests/test_ec.py
@@ -23,8 +23,7 @@ baseurl = "https://dd.weather.gc.ca"
                 "time": datetime(2016, 1, 15, 21),
             },
             (
-                f"{baseurl}/observations/xml/BC/yesterday/"
-                f"yesterday_bc_20160115_e.xml"
+                f"{baseurl}/20160115/WXO-DD/observations/xml/BC/yesterday/yesterday_bc_20160115_e.xml"
             ),
         ),
         (
@@ -35,25 +34,30 @@ baseurl = "https://dd.weather.gc.ca"
                 "language": "e",
                 "time": datetime(2016, 1, 15, 21),
             },
-            (f"{baseurl}/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"),
+            (
+                f"{baseurl}/20160115/WXO-DD/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"
+            ),
         ),
         (
             "nofreq-BC-EN",
             {"province": "BC", "language": "e", "time": datetime(2016, 1, 15, 21)},
             (
-                f"{baseurl}/observations/xml/BC/yesterday/"
-                f"yesterday_bc_20160115_e.xml"
+                f"{baseurl}/20160115/WXO-DD/observations/xml/BC/yesterday/yesterday_bc_20160115_e.xml"
             ),
         ),
         (
             "hourly-noprov-EN",
             {"freq": "hourly", "language": "e", "time": datetime(2016, 1, 15, 21)},
-            (f"{baseurl}/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"),
+            (
+                f"{baseurl}/20160115/WXO-DD/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"
+            ),
         ),
         (
             "hourly-BC-nolang",
             {"freq": "hourly", "province": "BC", "time": datetime(2016, 1, 15, 21)},
-            (f"{baseurl}/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"),
+            (
+                f"{baseurl}/20160115/WXO-DD/observations/xml/BC/hourly/hourly_bc_2016011521_e.xml"
+            ),
         ),
     ],
 )
@@ -69,7 +73,7 @@ def test_makeurl_no_time_hourly(mocker):
         url = makeurl(freq="hourly")
 
     assert url == (
-        f"{baseurl}/observations/xml/BC/hourly/hourly_bc_{t.strftime(fmt)}_e.xml"
+        f"{baseurl}/20160115/WXO-DD/observations/xml/BC/hourly/hourly_bc_{t.strftime(fmt)}_e.xml"
     )
 
 
@@ -81,7 +85,7 @@ def test_makeurl_no_time_daily(mocker):
         url = makeurl()
 
     assert url == (
-        f"{baseurl}/observations/xml/BC/"
+        f"{baseurl}/20160115/WXO-DD/observations/xml/BC/"
         f"yesterday/yesterday_bc_{t.strftime(fmt)}_e.xml"
     )
 
